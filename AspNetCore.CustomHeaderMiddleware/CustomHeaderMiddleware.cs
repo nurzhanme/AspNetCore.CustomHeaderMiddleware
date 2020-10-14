2d6322da2f30dbc8e1da27f5a89d0efdd83f1167
Нуржан Айтбаев <Aitbayev_N@server.jurinfo.zakon.kz>
2020-10-14 10:37:19 +0600

    Init commit

---
+++ AspNetCore.CustomHeaderMiddleware/CustomHeaderMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCore.CustomHeaderMiddleware
+{
+    public class CustomHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext context, string key, string value)
+        {
+            
+            context.Response.Headers.Add(key, value);
+            return _next(context);
+        }
+    }
+}
